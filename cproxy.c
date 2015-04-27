d103be49a9e4d884a4b21e72e8aa72afde4c3f9e
Nikolas J Gelo <ngelo@cs.arizona.edu>
2015-04-27 14:09:22 -0700

    Added usage printing if the required amount of command line args are not passed.

--- cproxy.c
+++ cproxy.c
@@ -4,10 +4,25 @@
 // CSC 425
 // Project 3
 
+void printUsage();
+
 int main(int argc, char *argv[])
 {
 	// TODO: This is where we start the client.
 
+	// Get the IP Address that was passed in.
+	if (argc < 2) {
+		printUsage();
+		return 1;
+	}
+
+	
+
 	return 0;
+}
+
+void printUsage()
+{
+	printf("./cproxy [w.x.y.z]\n");
 }
 
