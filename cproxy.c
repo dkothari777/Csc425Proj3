239d07872a69d502864477cdc4a04afe64210149
Nikolas J Gelo <ngelo@cs.arizona.edu>
2015-04-27 15:32:22 -0700

    Added basic setup for local telnet connection.

--- cproxy.c
+++ cproxy.c
@@ -12,9 +12,12 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+struct sockaddr_in LocalTelnetAddress;
+struct sockaddr_in SproxyAddress;
+
 void printUsage(FILE *stream);
 int setUpLocalTelnetConnection();
-int setUpSproxyConnection(struct sockaddr_in sproxyAddress);
+int setUpSproxyConnection();
 
 int main(int argc, char *argv[])
 {
@@ -24,20 +27,9 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// Set up the server's address information (sproxy).
-	struct sockaddr_in sproxyAddress;
-	memset(&sproxyAddress, 0, sizeof(sproxyAddress));
-	sproxyAddress.sin_family = AF_INET;
-	sproxyAddress.sin_port = htons(6200); // The server listens on port 6200.
-	if (inet_pton(AF_INET, sproxyAddress, &sproxyAddress.sin_addr) < 1) {
-		fprintf(stderr, "Error parsing sproxy IP Address.\n");
-		printUsage(stderr);
-		exit(EXIT_FAILURE);
-	}
-
-	// Set up the sproxy & local telnet connections.
-	int sproxySocketFileDescriptor = setUpSproxyConnection(sproxyAddress);
+	// Set up the local telnet & sproxy connections.
 	int localTelnetSocketFileDescriptor = setUpLocalTelnetConnection();
+	int sproxySocketFileDescriptor = setUpSproxyConnection(sproxyAddress);
 
 	// Connect to the server (sproxy).
 	if (connect(sproxySocketFileDescriptor, (struct sockaddr *) &sproxyAddress, sizeof(sproxyAddress)) < 0) {
@@ -56,13 +48,38 @@ void printUsage(FILE *stream)
 
 int setUpLocalTelnetConnection()
 {
-	// TODO: Finish the Telnet connection setup.
+	memset(&LocalTelnetAddress, 0, sizeof(LocalTelnetAddress)); // 0 out the struct
+	LocalTelnetAddress.sin_family = AF_INET; // Domain is the internet.
+	LocalTelnetAddress.sin_addr.s_addr = INADDR_ANY; 	//any IP address will do
+	LocalTelnetAddress.sin_port = htons(6200); // cproxy listens for telnet on port 5200
 
-	return 0;
+	int localTelnetSocketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+	if (localTelnetSocketFileDescriptor < 0) {
+		fprintf(stderr, "ERROR opening socket.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Bind socket to an address
+	if(bind(localTelnetSocketFileDescriptor, (struct sockaddr *) &LocalTelnetAddress, sizeof(localTelnetAddress)) < 0) {
+		fprintf(stderr, "ERROR on bind\n");
+		exit(EXIT_FAILURE);	
+	}
+
+	return localTelnetSocketFileDescriptor;
 }
 
-int setUpSproxyConnection(struct sockaddr_in sproxyAddress)
+int setUpSproxyConnection()
 {
+	// Set up the server's address information (sproxy).
+	memset(&SproxyAddress, 0, sizeof(SproxyAddress)); // 0 out the struct
+	SproxyAddress.sin_family = AF_INET; // Domain is the internet.
+	SproxyAddress.sin_port = htons(6200); // The server listens on port 6200.
+	if (inet_pton(AF_INET, SproxyAddress, &SproxyAddress.sin_addr) < 1) {
+		fprintf(stderr, "Error parsing sproxy IP Address.\n");
+		printUsage(stderr);
+		exit(EXIT_FAILURE);
+	}
+
 	// Create the socket.
 	int sproxySocketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 	if (sproxySocketFileDescriptor < 0) {
