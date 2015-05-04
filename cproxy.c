9df916ac73dd08239718b04ab4560c9822b3d6ba
Nikolas J Gelo <ngelo@cs.arizona.edu>
2015-05-03 21:13:20 -0700

    Added new lines to debug prints for easier reading.

--- cproxy.c
+++ cproxy.c
@@ -57,33 +57,29 @@ int main(int argc, char *argv[])
 		// Receive from local telnet session.
 		printf("Will receive from local telnet session.\n");
 		bytesReceived = recv(localTelnetSession, localTelnetBuffer, sizeof(uint32_t), 0);
-		printf("Did receive from local telnet session.\n");
+		printf("Did receive from local telnet session.\n\n");
 		if (bytesReceived < 0) {
 			fprintf(stderr, "ERROR on reading from local telnet session.\n");
 			exit(EXIT_FAILURE);
 		} else {
-			printf("Received a packet on local telnet session!\n");
-
 			// Forward buffer to the server telnet daemon.
 			printf("Will send to server telnet session.\n");
 			send(serverTelnetSocketDescriptor, localTelnetBuffer, sizeof(uint32_t), 0);
-			printf("Did send to server telnet session.\n");
+			printf("Did send to server telnet session.\n\n");
 		}
 
 		// Receive from server telnet daemon.
 		printf("Will receive from server telnet session.\n");
 		bytesReceived = recv(serverTelnetSocketDescriptor, serverTelnetBuffer, sizeof(uint32_t), 0);
-		printf("Did receive from server telnet session.\n");
+		printf("Did receive from server telnet session.\n\n");
 		if (bytesReceived < 0) {
 			fprintf(stderr, "ERROR on reading from server telnet session.\n");
 			exit(EXIT_FAILURE);
 		} else {
-			printf("Received a packet on server telnet session!\n");
-
 			// Forward buffer to the local telnet daemon.
 			printf("Will send to local telnet session.\n");
 			send(localTelnetSession, serverTelnetBuffer, sizeof(uint32_t), 0);
-			printf("Did send to local telnet session.\n");
+			printf("Did send to local telnet session.\n\n");
 		}
 	}
 
